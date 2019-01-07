4a30884ce18011c90435b8279e26bcbd39192aa5
sumin <cotack@naver.com>
2019-01-07 14:33:46 +0900

    sowl exam 19.01.07

--- app/src/main/java/net/skhu/exam1/activity_nutee.java
+++ app/src/main/java/net/skhu/exam1/activity_nutee.java
@@ -1,7 +1,13 @@
 package net.skhu.exam1;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class activity_nutee extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class activity_nutee extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nutee);
+
+        TextView textView = (TextView)findViewById(R.id.nutee_content);
+
+        Button button1 = (Button)findViewById(R.id.nutee_button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://nutee.kr/index.do"));
+                startActivity(intent);
+            }
+        });
     }
 }
